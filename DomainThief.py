00d7c382026b8bba92e99c0c13ca1b585da59d39
Tom Black <54150600+TomBlackOfficial@users.noreply.github.com>
2021-09-04 12:21:43 -0700

    Update DomainThief.py

--- DomainThief.py
+++ DomainThief.py
@@ -1,21 +1,41 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup, SoupStrainer
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-import time
+import requests
+
+counter = 0
 
 options = Options()
 options.add_argument("--incognito")
-browser = webdriver.Chrome(executable_path=r'chromedriver.exe', options=options)
-# --| Parse or automation
-browser.get('https://etherscan.io/enslookup-search?search=tomblack.eth')
-only_divs = SoupStrainer("div", {"class": "col-md-3 font-weight-bold font-weight-sm-normal mb-1 mb-md-0"})
-soup = BeautifulSoup(browser.page_source, 'lxml', parse_only=only_divs)
-browser.implicitly_wait(2)
 
-count = len(soup.find_all("div", {"class": "col-md-3 font-weight-bold font-weight-sm-normal mb-1 mb-md-0"}))
+url = "https://alter.com/blog/top-domain-name-sales"
+requests_session = requests.Session()
+source_code = requests_session.get(url)
+plain_text = source_code.text
+soup1 = BeautifulSoup(plain_text, 'lxml')
 
-if count > 0:
-    print("Taken")
-else:
-    print("Available")
+gdp_table = soup1.find("table", attrs={"class": "table"})
+gdp_table_data = gdp_table.tbody.find_all("tr")
+
+for dataItem in gdp_table_data:
+    domain_raw = dataItem.find_all("td")[1].text
+    if ".com" in domain_raw:
+        domain = domain_raw[:len(domain_raw) - 4]
+        if len(domain) > 2:
+            counter += 1
+            print(counter)
+
+            if counter > 383:
+                browser = webdriver.Chrome(executable_path=r'chromedriver.exe', options=options)
+                browser.get('https://etherscan.io/enslookup-search?search=' + domain + '.eth')
+
+                only_divs = SoupStrainer("div", {"class": "col-md-3 font-weight-bold font-weight-sm-normal mb-1 mb-md-0"})
+                soup2 = BeautifulSoup(browser.page_source, 'lxml', parse_only=only_divs)
+                browser.implicitly_wait(2)
+
+                count = len(soup2.find_all("div", {"class": "col-md-3 font-weight-bold font-weight-sm-normal mb-1 mb-md-0"}))
+
+                if count == 0:
+                    print(domain + ".eth" + " is Available")
+
+                browser.close()
